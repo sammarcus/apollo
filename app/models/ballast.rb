855e6650a9a9680ce98c1f60d8d3c9a8e73a70e2
sammarcus <sam.marcus@me.com>
2015-09-17 14:03:46 -0400

    added ballast model

---
+++ app/models/ballast.rb
@@ -0,0 +1,16 @@
+# Must change attributes
+
+class Ballast < ActiveRecord::Base
+
+  include AlgoliaSearch
+  algoliasearch per_environm
+
+  ent: true, enqueue: true do
+    attributesToIndex [:name, :email, :company, :city, :county, 'unordered(address)',
+      :state, :zip, :phone, :fax, 'unordered(website)'] # order matters, used by the ranking
+    attributesForFaceting [:city, :company] # use these attributes for faceting
+    customRanking ['desc(followers)'] # nb_followers reflect the record popularity
+    queryType 'prefixAll' # prefix query on all words
+  end
+
+end
