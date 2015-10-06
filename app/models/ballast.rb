921aefb80c44ca4c6acdbe8efd01ae4fd31c7961
sammarcus <sam.marcus@me.com>
2015-10-06 16:15:35 -0400

    changed attribs in ballast model

--- app/models/ballast.rb
+++ app/models/ballast.rb
@@ -1,16 +1,15 @@
-# Must change attributes
-
 class Ballast < ActiveRecord::Base
 
   include AlgoliaSearch
-  algoliasearch per_environm
+  algoliasearch index_name: "apollo_development", per_environment: false, enqueue: true do
+  # algoliasearch index_name: "Contact_development", per_environment: true, enqueue: true do
 
-  ent: true, enqueue: true do
-    attributesToIndex [:name, :email, :company, :city, :county, 'unordered(address)',
-      :state, :zip, :phone, :fax, 'unordered(website)'] # order matters, used by the ranking
-    attributesForFaceting [:city, :company] # use these attributes for faceting
-    customRanking ['desc(followers)'] # nb_followers reflect the record popularity
-    queryType 'prefixAll' # prefix query on all words
+
+    # attributesToIndex [:name, :email, :company, :city, :county, 'unordered(address)',
+    #   :state, :zip, :phone, :fax, 'unordered(website)'] # order matters, used by the ranking
+    # attributesForFaceting [:city, :company] # use these attributes for faceting
+    # customRanking ['desc(followers)'] # nb_followers reflect the record popularity
+    # queryType 'prefixAll' # prefix query on all words
   end
 
 end
